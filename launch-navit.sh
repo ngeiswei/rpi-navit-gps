e2a48fe198694bca291357901b054eeb000ed43b
Nil Geisweiller <ngeiswei@gmail.com>
2024-05-24 18:59:17 +0300

    Update launch-navit.sh

--- launch-navit.sh
+++ launch-navit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd ~/rpi-navit-gps
-minicom -S start-gps.minicom > ~/navit-build/navit/gps0 &
-cd ~/navit-build/navit
+PREV_DIR=$(pwd)
+cd ~/Navit/rpi-navit-gps
+minicom -S start-gps.minicom > ~/Navit/gps0 &
+cd ~/Navit/navit-build/navit
 ./navit
 killall minicom
-cd ~
+cd "$PREV_DIR"
